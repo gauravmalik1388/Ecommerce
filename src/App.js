6c8daaf3fcf44e7b81b47b7851235381d452fb22
Gaurav Malik <39573289+gauravmalik1388@users.noreply.github.com>
2020-05-23 15:15:33 -0400

    Ecommerce store using React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import Homepage from './homepage.componenet';
+import './App.css';
+
+function App() {
+  return (
+   <Homepage/>
+  );
+}
+
+export default App;
