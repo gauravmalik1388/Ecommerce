c280826769461d6d5da0df39545ec55e28c40082
Gaurav Malik <39573289+gauravmalik1388@users.noreply.github.com>
2020-05-26 09:04:34 -0400

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
-import Homepage from './homepage.componenet';
+import Homepage from './pages/homepage/homepage.componenet';
 import './App.css';
+import {
+  Switch,
+  Route
+  
+} from "react-router-dom";
+
+const Hatspage=()=>(
+
+<div>
+  <h1>Hats Page
+
+</h1>
+
+  </div>
+
+
+);
 
 function App() {
   return (
-   <Homepage/>
+   
+
+<div>
+<Switch>
+<Route exact path='/' component={Homepage}  />
+<Route exact path='/Hats' component={Hatspage}  />
+</Switch>
+</div>
+
+
+
   );
 }
 
---
+++ src/pages/homepage/homepage.componenet.jsx
@@ -0,0 +1,24 @@
+import React from 'react' ;
+
+import './homepage.styles.scss';
+import Directory from '../../components/Directory/directory.components.jsx'
+
+const Homepage =()=>(
+
+<div className='homepage'>
+<Directory/>
+</div>
+
+
+
+
+
+
+
+
+
+)
+
+
+
+export default Homepage ;
